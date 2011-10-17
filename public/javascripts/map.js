ad8bfdfeea3e73c1a1b09ff78190e7b9d76ff44f
Márton Salomváry <salomvary@gmail.com>
2011-10-17 10:57:48 +0200

    display map as well

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,19 +4,32 @@ var Map = Backbone.View.extend({
 
 	defaultLocation: new google.maps.LatLng(47.3, 19.5),
 
-	initialize: function() {
+	initialize: function(options) {
 		var container = $('<div/>', {css: {width:'100%', height:'100%'}})
-			.appendTo(this.el);
-		var center = google.loader.ClientLocation 
-			?	new google.maps.LatLng(
-				google.loader.ClientLocation.latitude, 
-				google.loader.ClientLocation.longitude)
-			: this.defaultLocation;
+			.appendTo(this.el),
+			center;
+		if(options.center) {
+			center = new google.maps.LatLng(
+				options.center.lat,
+				options.center.lon);
+		} else if(google.loader.ClientLocation) {
+			center = new google.maps.LatLng(
+				google.loader.ClientLocation.latitude,
+				google.loader.ClientLocation.longitude);
+		} else {
+			center = this.defaultLocation;
+		}
 		this.map = new google.maps.Map(container[0], {
-			zoom: 8,
+			zoom: (options.center && options.center.zoom) ? options.center.zoom :  8,
 			center: center,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		});
+		if(options.marker) {
+			this.marker = new google.maps.Marker({
+				map: this.map
+			});
+			this.marker.setPosition(center);
+		}
 	}	
 });
 
--- public/javascripts/show.js
+++ public/javascripts/show.js
@@ -1,13 +1,15 @@
+define(['map'], function(Map) {
+
 app.Show = Backbone.View.extend({
 	el: $('#show'),
 	initialize: function() {
 		var data = this.el.data();
-		app.requireMaps($.proxy(function() {
-			this.map = new google.maps.Map(this.el[0], {
-				zoom: data.zoom,
-				center: new google.maps.LatLng(data.lat, data.lon),
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			});
-		}, this));
+		this.map = new Map({
+			el: this.el,
+			center: data,
+			marker: true
+		});
 	}
+});
+
 });
